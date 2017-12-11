a2595d26b2225b0dc53668b464a1044bda376841
Zolbayar.b <Zolbayar.b@skytel.mn>
2017-12-11 17:53:45 +0800

    i18n added

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ var express = require('express'),
 	session = require('express-session'),
   expressValidator = require('express-validator'),
   bodyParser = require('body-parser'),
-  path = require("path");
+  path = require("path"),
+  i18n = require("i18n");
+
+i18n.configure({
+    locales:['en', 'mn'],
+    directory: path.join(__dirname, "locales")
+});
 
 const NodeCache = require( "node-cache" );
 const myCache = new NodeCache( { stdTTL: 600, checkperiod: 620 } );
@@ -16,7 +22,7 @@ var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(session({
@@ -25,11 +31,22 @@ app.use(session({
 	saveUninitialized: true
 }));
 
+app.use(i18n.init);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
+
+app.use(function(req, res, next) {
+    // express helper for natively supported engines
+    res.locals.__ = res.__ = function() {
+        console.log("i18n called");
+        return i18n.__.apply(req, arguments);
+    };
+
+    next();
+});
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -38,6 +55,8 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/node_modules', express.static(process.cwd() + '/node_modules'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
+
+
 
 routes(app, passport, myCache);
 
