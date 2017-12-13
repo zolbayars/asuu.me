fd640b1b29d636cb2fafe1b79b860a0e36271d0b
Zolbayar.b <Zolbayar.b@skytel.mn>
2017-12-14 07:01:49 +0800

    views added - home page, lists

--- server.js
+++ server.js
@@ -7,28 +7,7 @@ var express = require('express'),
 	session = require('express-session'),
   expressValidator = require('express-validator'),
   bodyParser = require('body-parser'),
-  path = require("path"),
-  i18n = require("i18n");
-
-i18n.configure({
-    locales:['mn', 'en'],
-    defaultLocale: 'mn',
-    queryParameter: 'lang',
-    directory: path.join(__dirname, "locales"),
-    logDebugFn: function (msg) {
-        console.log('debug', msg);
-    },
-
-    // setting of log level WARN - default to require('debug')('i18n:warn')
-    logWarnFn: function (msg) {
-        console.log('warn', msg);
-    },
-
-    // setting of log level ERROR - default to require('debug')('i18n:error')
-    logErrorFn: function (msg) {
-        console.log('error', msg);
-    },
-});
+  path = require("path");
 
 const NodeCache = require( "node-cache" );
 const myCache = new NodeCache( { stdTTL: 600, checkperiod: 620 } );
@@ -37,7 +16,7 @@ var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(session({
@@ -46,22 +25,11 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-app.use(i18n.init);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
-
-app.use(function(req, res, next) {
-    // express helper for natively supported engines
-    res.locals.__ = res.__ = function() {
-        res.setLocale('mn');
-        return i18n.__.apply(req, arguments);
-    };
-
-    next();
-});
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -70,8 +38,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/node_modules', express.static(process.cwd() + '/node_modules'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
-
-
 
 routes(app, passport, myCache);
 
