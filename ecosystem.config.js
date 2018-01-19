7beb2754991ea35f09e7ccd3ebfe2f32e08b54fb
zolbayars <zolube@gmail.com>
2018-01-19 14:50:58 +0800

    service config

---
+++ ecosystem.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'asuu.me',
+      script    : 'server.js',
+      args      : '-i 0'
+      exec_mode : 'cluster'
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  // deploy : {
+  //   production : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/production',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  //   },
+  //   dev : {
+  //     user : 'node',
+  //     host : '212.83.163.1',
+  //     ref  : 'origin/master',
+  //     repo : 'git@github.com:repo.git',
+  //     path : '/var/www/development',
+  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+  //     env  : {
+  //       NODE_ENV: 'dev'
+  //     }
+  //   }
+  // }
+};
